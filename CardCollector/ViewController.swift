7304194c3264df6ec784ce0ce804384ef539e8a2
Brian Jeong <bj254@cornell.edu>
2025-03-04 21:43:48 -0500

    Initial Commit

---
+++ CardCollector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardCollector
+//
+//  Created by Brian Jeong on 3/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
